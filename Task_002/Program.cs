9a43a383da0ab0b23ff8cb29e8c06ab2c0ad09df
Dmitry <kochetov.dmitry@mail.ru>
2022-07-08 21:59:29 +0300

    небольшие правки

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -6,37 +6,37 @@ if (num1 == 1)
     Console.WriteLine("Вы выбрали понедельник");
 }
 
-if (num1 == 2)
+else if (num1 == 2)
 {
     Console.WriteLine("Вы выбрали вторник");
 }
 
-if (num1 == 3)
+else if (num1 == 3)
 {
     Console.WriteLine("Вы выбрали среду");
 }
 
-if (num1 == 4)
+else if (num1 == 4)
 {
     Console.WriteLine("Вы выбрали четверг");
 }
 
-if (num1 == 5)
+else if (num1 == 5)
 {
     Console.WriteLine("Вы выбрали пятницу");
 }
 
-if (num1 == 6)
+else if (num1 == 6)
 {
     Console.WriteLine("Вы выбрали субботу");
 }
 
-if (num1 == 7)
+else if (num1 == 7)
 {
     Console.WriteLine("Вы выбрали Воскресенье");
 }
 
-if (num1 > 7)
+else
 {
     Console.WriteLine("Вы ввели неправильное число");
 }
